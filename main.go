164ce53eddce3f86e536afbd59d2922a6f1affce
lucia zhang <kofeng@ebay.com>
2022-06-30 15:58:15 -0700

    update main and add user.go

--- main.go
+++ main.go
@@ -4,10 +4,16 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"reflect"
 	"strconv"
+
+	"cloud.google.com/go/storage"
+	jwtmiddleware "github.com/auth0/go-jwt-middleware"
+	"github.com/form3tech-oss/jwt-go"
+	"github.com/gorilla/mux"
 
 	"github.com/olivere/elastic/v7"
 	"github.com/pborman/uuid"
@@ -22,14 +28,19 @@ type Post struct {
 	User     string   `json:"user"`
 	Message  string   `json:"message"`
 	Location Location `json:"location"`
+	Url      string   `json:"url"`
 }
 
 const (
 	INDEX    = "around"
 	TYPE     = "post"
 	DISTANCE = "200km"
-	ES_URL   = "http://34.125.206.199:9200"
+	ES_URL   = "http://34.125.134.20:9200"
+
+	BUCKET_NAME = "post-images-352203"
 )
+
+var mySigningKey = []byte("secret")
 
 func main() {
 	// Create a client
@@ -46,16 +57,12 @@ func main() {
 	if !exists {
 		// Create a new index.
 		mapping := `{
-				"mappings":{
-					
-					
+				"mappings":{				
 						"properties":{
 							"location":{
 								"type":"geo_point"
 							}
-						}
-					
-					
+						}	
 				}
 			}`
 		_, err := client.CreateIndex(INDEX).Body(mapping).Do(context.Background())
@@ -65,24 +72,102 @@ func main() {
 		}
 	}
 	fmt.Println("Start service")
-	http.HandleFunc("/post", handlePost)
-	http.HandleFunc("/search", handleSearch)
+
+	r := mux.NewRouter()
+
+	var jwtMiddleware = jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			return mySigningKey, nil
+		},
+		SigningMethod: jwt.SigningMethodHS256,
+	})
+
+	r.Handle("/post", jwtMiddleware.Handler(http.HandlerFunc(handlePost))).Methods("POST")
+	r.Handle("/search", jwtMiddleware.Handler(http.HandlerFunc(handleSearch))).Methods("GET")
+	r.Handle("/login", http.HandlerFunc(loginHandler)).Methods("POST")
+	r.Handle("/signup", http.HandlerFunc(signupHandler)).Methods("POST")
+
+	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func handlePost(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Received one post request.")
+	w.Header().Set("Content-Type", "Application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow_Headers", "Content-Type,Authorization")
 
-	decoder := json.NewDecoder(r.Body)
-	var p Post
-	if err := decoder.Decode(&p); err != nil {
+	user := r.Context().Value("user")
+	claims := user.(*jwt.Token).Claims
+	username := claims.(jwt.MapClaims)["username"]
+
+	r.ParseMultipartForm(32 << 20)
+
+	// Parse form data
+	fmt.Printf("Received one post request %s\n", r.FormValue("message"))
+	lat, _ := strconv.ParseFloat(r.FormValue("lat"), 64)
+	lon, _ := strconv.ParseFloat(r.FormValue("lon"), 64)
+
+	p := &Post{
+		User:    username.(string),
+		Message: r.FormValue("message"),
+		Location: Location{
+			Lat: lat,
+			Lon: lon,
+		},
+	}
+
+	id := uuid.New()
+
+	file, _, err := r.FormFile("image")
+	if err != nil {
+		http.Error(w, "FCS is not setup", http.StatusInternalServerError)
+		fmt.Printf("GCS is not setup %v\n", err)
+		panic(err)
+	}
+	defer file.Close()
+
+	ctx := context.Background()
+
+	_, attrs, err := saveToGCS(ctx, file, BUCKET_NAME, id)
+	if err != nil {
+		http.Error(w, "GCS is not setup", http.StatusInternalServerError)
+		fmt.Printf("GCS is not setup %v\n", err)
 		panic(err)
 	}
 
-	fmt.Fprintf(w, "Post received: %s\n", p.Message)
+	p.Url = attrs.MediaLink
 
-	id := uuid.New()
-	saveToES(&p, id)
+	saveToES(p, id)
+}
+
+func saveToGCS(ctx context.Context, r io.Reader, bucketName, name string) (*storage.ObjectHandle, *storage.ObjectAttrs, error) {
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	bucket := client.Bucket(bucketName)
+	if _, err := bucket.Attrs(ctx); err != nil {
+		return nil, nil, err
+	}
+
+	obj := bucket.Object(name)
+	wc := obj.NewWriter(ctx)
+	if _, err = io.Copy(wc, r); err != nil {
+		return nil, nil, err
+	}
+	if err := wc.Close(); err != nil {
+		return nil, nil, err
+	}
+
+	// if err := obj.ACL().Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
+	// 	return nil, nil, err
+	// }
+
+	attrs, err := obj.Attrs(ctx)
+	fmt.Printf("Post is saved to GCS: %s \n", attrs.MediaLink)
+	return obj, attrs, err
+
 }
 
 func saveToES(p *Post, id string) {
---
+++ user.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	elastic "github.com/olivere/elastic/v7"
+
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"reflect"
+	"regexp"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+const (
+	TYPE_USER = "user"
+)
+
+var (
+	usernamePattern = regexp.MustCompile(`^[a-z0-9_]+$`).MatchString
+)
+
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Age      int    `json:"age"`
+	Gender   string `json:"gender"`
+}
+
+// checkIser check whether user is valid
+func checkUser(username, password string) bool {
+	es_client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		fmt.Printf("ES is not setup %v\n", err)
+		panic(err)
+	}
+
+	termQuery := elastic.NewTermQuery("username", username)
+	queryResult, err := es_client.Search().
+		Index(INDEX).
+		Query(termQuery).
+		Pretty(true).
+		Do(context.Background())
+	if err != nil {
+		fmt.Printf("ES query failed %v\n", err)
+		panic(err)
+	}
+
+	var tyu User
+	for _, item := range queryResult.Each(reflect.TypeOf(tyu)) {
+		u := item.(User)
+		return u.Password == password && u.Username == username
+	}
+
+	return false
+
+}
+
+// Add a new user, Return true if successfully
+func addUser(user User) bool {
+	es_client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		fmt.Printf("ES is not setup %v\n", err)
+		return false
+	}
+
+	termQuery := elastic.NewTermQuery("username", user.Username)
+	queryResult, err := es_client.Search().
+		Index(INDEX).
+		Query(termQuery).
+		Pretty(true).Do(context.Background())
+	if err != nil {
+		fmt.Printf("ES query failed %v\n", err)
+		return false
+	}
+
+	if queryResult.TotalHits() > 0 {
+		fmt.Printf("User %s already exists, cannot create dulplicate user. \n", user.Username)
+		return false
+	}
+
+	_, err = es_client.Index().
+		Index(INDEX).
+		Id(user.Username).
+		BodyJson(user).
+		Refresh("true").
+		Do(context.Background())
+	if err != nil {
+		fmt.Printf("ES save user falied %v\n", err)
+		return false
+	}
+
+	return true
+}
+
+// if signup success, a new session is created
+func signupHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Receive a signup request")
+
+	decoder := json.NewDecoder(r.Body)
+	var u User
+	if err := decoder.Decode(&u); err != nil {
+		panic(err)
+	}
+
+	if u.Username != "" && u.Password != "" && usernamePattern(u.Username) {
+		if addUser(u) {
+			fmt.Println("User added successfully.")
+			w.Write([]byte("User added successfully."))
+		} else {
+			fmt.Println("Failed to add a new user")
+			http.Error(w, "Failed to add a new user", http.StatusInternalServerError)
+		}
+	} else {
+		fmt.Printf("Empty password or username or valid username.")
+		http.Error(w, "Empty password or username or valid username.", http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received one login request")
+
+	decoder := json.NewDecoder(r.Body)
+	var u User
+	if err := decoder.Decode(&u); err != nil {
+		panic(err)
+		return
+	}
+
+	if checkUser(u.Username, u.Password) {
+		token := jwt.New(jwt.SigningMethodHS256)
+		claims := token.Claims.(jwt.MapClaims)
+		/* Set token claims */
+		claims["username"] = u.Username
+		claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+
+		/* Sign the token with our secret */
+		tokenString, _ := token.SignedString(mySigningKey)
+
+		/* Finally, write the token to the browser window */
+		w.Write([]byte(tokenString))
+	} else {
+		fmt.Println("Invalid password or username.")
+		http.Error(w, "Invalid password or username", http.StatusForbidden)
+	}
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+}
